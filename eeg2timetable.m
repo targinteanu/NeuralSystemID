9448723d1f0aa3eeb56fc48b1fc3d3ecc9ee712e
Toren Arginteanu <torenarginteanu@Torens-MacBook-Pro.local>
2024-05-18 17:41:50 -0400

    all channel names upper case in tables, for consistency across subjects

--- eeg2timetable.m
+++ eeg2timetable.m
@@ -3,6 +3,7 @@ function TT = eeg2timetable(EEG)
 t = milliseconds(EEG.times');
 X = EEG.data'; 
 lbl = {EEG.chanlocs.labels};
+lbl = upper(lbl);
 
 TT = array2timetable(X,"RowTimes",t,"VariableNames",lbl); 
 
