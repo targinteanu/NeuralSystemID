0b08ffb7fa0e2b2f8030a328205b1d378fd643bf
Toren Arginteanu <torenarginteanu@torens-mbp.acasmart.jh.edu>
2024-07-02 12:07:06 -0400

    working internally with arrays to avoid back-and-forth conversion to tables

--- fitLTIauton.m
+++ fitLTIauton.m
@@ -9,33 +9,35 @@ Xtrain = table2array(trainData)';
 X1 = Xtrain(:,1:(end-1)); X2 = Xtrain(:,2:end); 
 A = X2*X1' * (X1*X1')^-1;
 
-trainPred = trainData; 
-for t = 2:height(trainPred)
-    %x0 = table2array(trainPred(t-1,:))'; 
-    x0 = table2array(trainData(t-1,:))';
+trainPredX = Xtrain; 
+for t = 2:width(trainPredX)
+    %x0 = trainPredX(:,t-1); 
+    x0 = Xtrain(:,t-1);
     x = A*x0; 
-    trainPred{t,:} = x';
+    trainPredX(:,t) = x;
 end
 
 if ~isempty(testData)
-%testPred = trainData;
-testPred = testData;
-%testPred{1,:} = x';
-%testPred{1,:} = table2array(trainData(end,:));
-for t = 2:height(testPred)
-    %x0 = table2array(testPred(t-1,:))'; 
-    x0 = table2array(testData(t-1,:))';
+Xtest = table2array(testData)';
+testPredX = Xtest;
+%testPredX = Xtrain;
+%testPredX(:,1) = x;
+%testPredX(:,1) = Xtrain(:,end);
+for t = 2:width(testPredX)
+    %x0 = testPredX(:,t-1); 
+    x0 = Xtest(:,t-1);
     x = A*x0; 
-    testPred{t,:} = x';
+    testPredX(:,t) = x;
 end
+testPred = testData; 
+testPred{:,:} = testPredX';
 %t = testPred.Time; 
 %t = t - t(1) + trainPred.Time(end); 
 %testPred.Time = t;
-testPredX = table2array(testPred(:,:))'; 
-Xtest = table2array(testData(:,:))'; 
 end
 
-trainPredX = table2array(trainPred(:,:))'; 
+trainPred = trainData; 
+trainPred{:,:} = trainPredX';
 
 trainEval.RMSE = rmse(Xtrain(:), trainPredX(:)); 
 trainEval.pRMSE = rmse(Xtrain(:), trainPredX(:))/rms(Xtrain(:));
