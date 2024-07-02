1c15fc7bb8c68c285c934324305cdaf25084c2ce
Toren Arginteanu <torenarginteanu@torens-mbp.acasmart.jh.edu>
2024-07-02 11:47:49 -0400

    began replacing brace indexing to improve speed

--- fitLTIauton.m
+++ fitLTIauton.m
@@ -5,14 +5,14 @@ if nargin < 2
     testData = [];
 end
 
-Xtrain = trainData{:,:}'; 
+Xtrain = table2array(trainData)'; 
 X1 = Xtrain(:,1:(end-1)); X2 = Xtrain(:,2:end); 
 A = X2*X1' * (X1*X1')^-1;
 
 trainPred = trainData; 
 for t = 2:height(trainPred)
-    %x0 = trainPred{t-1,:}'; 
-    x0 = trainData{t-1,:}';
+    %x0 = table2array(trainPred(t-1,:))'; 
+    x0 = table2array(trainData(t-1,:))';
     x = A*x0; 
     trainPred{t,:} = x';
 end
@@ -21,21 +21,21 @@ if ~isempty(testData)
 %testPred = trainData;
 testPred = testData;
 %testPred{1,:} = x';
-%testPred{1,:} = trainData{end,:};
+%testPred{1,:} = table2array(trainData(end,:));
 for t = 2:height(testPred)
-    %x0 = testPred{t-1,:}'; 
-    x0 = testData{t-1,:}';
+    %x0 = table2array(testPred(t-1,:))'; 
+    x0 = table2array(testData(t-1,:))';
     x = A*x0; 
     testPred{t,:} = x';
 end
 %t = testPred.Time; 
 %t = t - t(1) + trainPred.Time(end); 
 %testPred.Time = t;
-testPredX = testPred{:,:}'; 
-Xtest = testData{:,:}'; 
+testPredX = table2array(testPred(:,:))'; 
+Xtest = table2array(testData(:,:))'; 
 end
 
-trainPredX = trainPred{:,:}'; 
+trainPredX = table2array(trainPred(:,:))'; 
 
 trainEval.RMSE = rmse(Xtrain(:), trainPredX(:)); 
 trainEval.pRMSE = rmse(Xtrain(:), trainPredX(:))/rms(Xtrain(:));
