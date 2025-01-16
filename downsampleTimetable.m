eedd879c12ddfde4e4932ef17650be5676da55f0
Toren Arginteanu <torenarginteanu@Torens-MacBook-Pro.local>
2025-01-16 02:11:15 -0500

    new network architecture and preprocessing

---
+++ downsampleTimetable.m
@@ -0,0 +1,17 @@
+function tbl = downsampleTimetable(TBL, rat) 
+% Downsample an input <TBL> with sampling ratio <rat> such that the output
+% table <tbl> will have one (averaged) entry for every <rat> input entries;
+% <rat> should be an integer.  
+rat = round(rat); 
+M = movmean(TBL.Variables, rat); 
+m = downsample(M, rat);
+t = downsample(TBL.Time, rat); 
+tbl = array2timetable(m, "RowTimes",t, ...
+    "VariableNames",TBL.Properties.VariableNames);
+tbl.Properties.CustomProperties = TBL.Properties.CustomProperties; 
+tbl.Properties.UserData = TBL.Properties.UserData; 
+tbl.Properties.VariableUnits = TBL.Properties.VariableUnits;
+tbl.Properties.VariableDescriptions = TBL.Properties.VariableDescriptions; 
+tbl.Properties.Events = TBL.Properties.Events; % need to downsample this as well??
+tbl.Properties.Description = [TBL.Properties.Description,' downsampled'];
+end
