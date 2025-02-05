e70ce0b441c24e2c71928a45ed6aab70c6485afa
Toren Arginteanu <torenarginteanu@Torens-MacBook-Pro.local>
2025-02-05 01:04:16 -0500

    exclude first k from evaluation metric

--- PD_auton_preprocessing.m
+++ PD_auton_preprocessing.m
@@ -67,6 +67,7 @@ end
 %}
 
 %% wavelet transform
+wCustom = [1.5, -1, .6, -.33, .22, -.1, .05, 0, -.01, .05];
 %N = 24; % vaid
 N = 18; % beyl 
 N = N/2;
@@ -79,7 +80,8 @@ for c = 1:width(dataBaseline)
     dataWavelet2.Properties.VariableNames{c} = ...
         [dataWavelet2.Properties.VariableNames{c},' Detail'];
 end
-dataBaseline = [dataWavelet1, dataWavelet2]; 
+%dataBaseline = [dataWavelet1, dataWavelet2]; 
+dataBaseline = dataWavelet2;
 
 %% downsample, but ensure above nyquist rate 
 fsNew = 2.1*hico;
@@ -151,30 +153,31 @@ end
 hzn = ceil(.25 * fsNew); % .25-second-ahead prediction horizon
 Lval = 1000; % length of validation data 
 ypTrain = myPredict(sysLTI, dataTrain(1:Lval,:), hzn, true);  
+ypTrain = ypTrain((hzn+1):end,:); 
 errsTrn = ...
-    rmse(ypTrain.Variables, dataTrain(1:Lval,:).Variables) ./ ...
-    rms(dataTrain(1:Lval,:).Variables);
+    rmse(ypTrain.Variables, dataTrain((hzn+1):Lval,:).Variables) ./ ...
+    rms(dataTrain((hzn+1):Lval,:).Variables);
 errTrn = mean( errsTrn );
-rhoTrn = arrayfun(@(c) corr(dataTrain{1:Lval,c}, ypTrain{:,c}), 1:width(dataTrain));
+rhoTrn = arrayfun(@(c) corr(dataTrain{(hzn+1):Lval,c}, ypTrain{:,c}), 1:width(dataTrain));
 [~,ch1] = min(errsTrn); % best channel
 [~,ch2] = min(abs(errTrn - errsTrn)); % most representative channel
 fig1 = figure('Units','normalized', 'Position',[.05 .05 .9 .9]); 
 
 subplot(3,2,1); 
-plottbl(dataTrain(1:Lval,:), ch1); grid on; 
+plottbl(dataTrain((hzn+1):Lval,:), ch1); grid on; 
 hold on; plottbl(ypTrain, ch1);
 title('best channel');
 subplot(3,2,3); 
-plottbl(dataTrain(1:Lval,:), ch2); grid on; 
+plottbl(dataTrain((hzn+1):Lval,:), ch2); grid on; 
 hold on; plottbl(ypTrain, ch2);
 title('most representative channel');
 
 subplot(3,2,2);
-plot(dataTrain{1:Lval,ch1}, ypTrain{:,ch1}, '.');
+plot(dataTrain{(hzn+1):Lval,ch1}, ypTrain{:,ch1}, '.');
 xlabel('actual'); ylabel('predicted'); grid on;
 title(dataTrain.Properties.VariableNames{ch1});
 subplot(3,2,4);
-plot(dataTrain{1:Lval,ch2}, ypTrain{:,ch2}, '.');
+plot(dataTrain{(hzn+1):Lval,ch2}, ypTrain{:,ch2}, '.');
 xlabel('actual'); ylabel('predicted'); grid on;
 title(dataTrain.Properties.VariableNames{ch2});
 
@@ -193,6 +196,7 @@ end
 %% testing results 
 dataTestCat = dataTest{1}(1:Lval,:);
 ypTestCat = myPredict(sysLTI, dataTestCat, hzn);
+dataTestCat = dataTestCat((hzn+1):end,:); ypTestCat = ypTestCat((hzn+1):end,:);
 fig2 = figure('Units','normalized', 'Position',[.05 .05 .9 .9]); 
 
 subplot(3,2,1);
@@ -214,8 +218,8 @@ xlabel('actual'); ylabel('predicted'); grid on;
 title(dataTestCat.Properties.VariableNames{ch2});
 
 errsTst = ...
-    rmse(ypTestCat.Variables, dataTestCat(1:Lval,:).Variables) ./ ...
-    rms(dataTestCat(1:Lval,:).Variables);
+    rmse(ypTestCat.Variables, dataTestCat.Variables) ./ ...
+    rms(dataTestCat.Variables);
 errTst = mean( errsTst );
 rhoTst = arrayfun(@(c) corr(dataTestCat{:,c}, ypTestCat{:,c}), 1:width(dataTestCat));
 subplot(3,1,3); stem(errsTst); grid on; 
