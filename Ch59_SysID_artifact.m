ba0a377f1329e64563f06bb3c26df37eab6c22f4
Toren Arginteanu <targinteanu@outlook.com>
2025-06-10 16:11:19 -0400

    Update Ch59_SysID_artifact.m
    moved AR and adaptive AR to separate function; needs to be added to plot

--- Ch59_SysID_artifact.m
+++ Ch59_SysID_artifact.m
@@ -30,37 +30,18 @@ dtaBL = dta(ind_bl_str:ind_bl_end,:);
 
 %% AR model 
 
-% best fit AR
+dta1ch = dta(:,chnum_to_plot);
+dtaBL1ch = dtaBL(:,chnum_to_plot);
+
+% best fit and dynamic AR
 N = 10; % order 
-ARmdl = ar(dtaBL(:,chnum_to_plot), N, 'yw');
-W0 = ARmdl.A; W01 = W0(1); W0 = W0(2:end); W0 = -W0/W01; % AR model wts 
-W0 = fliplr(W0);
-
-% setup for dynamic updating and plotting 
-stepsize = 1e-8;
-W1 = zeros(size(W0)); W = zeros(height(dtaBL), length(W1));
-figure; stem(W0); hold on; w1plt = stem(W1); grid on;
-legend('Yule-Walker', 'Adaptive');
-ttl = title(['Step 0 of ',num2str(height(dtaBL))]);
-pause(.1);
-progticksteps = 1000;
-
-% dynamic updating AR by online least squares gradient descent
-for t = (N+1):height(dtaBL)
-    y = dtaBL{t,chnum_to_plot}; 
-    x = dtaBL{(t-N):(t-1),chnum_to_plot};
-    del = x*(y-W1*x);
-    W1 = W1 + stepsize*del'; 
-    W(t,:) = W1;
-    if ~mod(t,progticksteps)
-        w1plt.YData = W1;
-        ttl.String = ['Step ',num2str(t),' of ',num2str(height(dtaBL))];
-        pause(.001);
-    end
-end
-
-% show 
-figure; imagesc([W; W0]); colorbar; 
+tic
+[aarBL,aarAll,aarTrnEval,aarTstEval,ARmdl] = ...
+    AdaptAR(dtaBL1ch,dta1ch,N,1e-12,Tr_thr,true);
+[arSO,arEval] = projAR(ARmdl,dta1ch,Tr_thr);
+toc
+aarTstEval
+arEval
 
 %% LTI fit sys ID 
 
