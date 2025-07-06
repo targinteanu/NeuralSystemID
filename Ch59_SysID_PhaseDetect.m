241fd0d34a5c9e2aedac2e926c9e8ccaad8a73e5
Toren Arginteanu <targinteanu@outlook.com>
2025-07-05 23:33:58 -0400

    Update Ch59_SysID_PhaseDetect.m
    made to run in loop through channels and aggregate results

--- Ch59_SysID_PhaseDetect.m
+++ Ch59_SysID_PhaseDetect.m
@@ -21,8 +21,6 @@ dta = ns2timetable(NS2);
 dta = dta(:,chincl); 
 dta = dta(1:ind_rec_end,:); 
 
-chtoplot = 'LS59';
-
 %% preprocess 
 
 %Fs = dta.Properties.SampleRate;
@@ -32,16 +30,25 @@ hpf = designfilt('highpassiir', 'SampleRate',Fs, 'DesignMethod','butter', ...
 dta = FilterTimetable(@(d,x) filtfilt(d,x), hpf, dta);
 dtaBL = dta(ind_bl_str:ind_bl_end,:);
 
-%% run phase detection 
-
 packetSize = 10; 
 phTarget = 0;
 ARwin = 1000; ARlen = 10; predWin = 100;
 
+errResults = cell(3, width(dtaBL), 2);
+% dim 1: phase est err, freq est err, phase target err 
+% dim 2: channel 
+% dim 3: const vs dynamic AR model 
+
+for c = 1:width(dtaBL)
+
+chtoplot = dtaBL.Properties.VariableNames{c}
+
+%% run phase detection 
+
 % with constant AR model: 
-[phAll, phEst, frAll, frEst] = ...
+[phAll, phEst, frAll, frEst, ~, phStimConst] = ...
     offline_PhaseDetect(dtaBL.(chtoplot)', Fs, [], dtaBL.Time', chtoplot, ...
-    phTarget, [13,30], ARwin, ARlen, predWin, -1, packetSize, -1, [], true);
+    phTarget, [13,30], ARwin, ARlen, predWin, -1, packetSize, -1, [], false);
 phErrConst = phEst-phAll; frErrConst = frEst - frAll;
 
 pause(.001); 
@@ -49,13 +56,18 @@ drawnow;
 pause(.001);
 
 % with dynamic AR model: 
-[phAll, phEst, frAll, frEst] = ...
+[phAll, phEst, frAll, frEst, ~, phStimDyn] = ...
     offline_PhaseDetect(dtaBL.(chtoplot)', Fs, [], dtaBL.Time', chtoplot, ...
-    phTarget, [13,30], ARwin, ARlen, predWin, -1, packetSize, .0001, true, true);
+    phTarget, [13,30], ARwin, ARlen, predWin, -1, packetSize, .0001, true, false);
 phErrDyn = phEst-phAll; frErrDyn = frEst - frAll;
 
+pause(.001); 
+drawnow;
+pause(.001);
+
 %% compare constant vs dynamic AR results
-figure; 
+
+figure; sgtitle(chtoplot);
 subplot(1,2,1); 
 polarhistogram(phErrConst); hold on; polarhistogram(phErrDyn);
 title('Phase error (causal - offline)'); 
@@ -71,3 +83,55 @@ legend( ...
     ['Constant - RMSE ',num2str(rms(frErrConst))], ... 
     ['Dynamic - RMSE ',num2str(rms(frErrDyn))], ...
     'Location','northoutside');
+
+pause(.001); 
+drawnow;
+pause(.001);
+
+errResults{1,c,1} = phErrConst; errResults{1,c,2} = phErrDyn;
+errResults{2,c,1} = frErrConst; errResults{2,c,2} = frErrDyn;
+errResults{3,c,1} = phStimConst-phTarget; 
+errResults{3,c,2} = phStimDyn-phTarget;
+
+end
+
+%% aggregate all channel results 
+
+errResultsAll = cell(size(errResults,1), size(errResults,3));
+for r = 1:size(errResults,1)
+    for h = 1:size(errResults,3)
+        for c = 1:size(errResults,2)
+            errResultsAll{r,h} = [errResultsAll{r,h}, errResults{r,c,h}];
+        end
+    end
+end
+
+figure; 
+subplot(1,3,1); 
+polarhistogram(errResultsAll{1,1}); hold on; polarhistogram(errResultsAll{1,2});
+title('Phase error (causal - offline)'); 
+legend( ...
+    ['Constant - RMSE ',num2str(rms(errResultsAll{1,1}))], ... 
+    ['Dynamic - RMSE ',num2str(rms(errResultsAll{1,2}))], ...
+    'Location','northoutside');
+[~,p] = ttest2(errResultsAll{1,1}.^2, errResultsAll{1,2}.^2, 'tail', 'right');
+subtitle(['p = ',num2str(p)])
+subplot(1,3,2); 
+histogram(errResultsAll{2,1}); hold on; grid on; histogram(errResultsAll{2,2});
+xlabel('frequency (Hz)'); 
+title('Freq. error (causal - offline)'); 
+legend( ...
+    ['Constant - RMSE ',num2str(rms(errResultsAll{2,1}))], ... 
+    ['Dynamic - RMSE ',num2str(rms(errResultsAll{2,2}))], ...
+    'Location','northoutside');
+[~,p] = ttest2(errResultsAll{2,1}.^2, errResultsAll{2,2}.^2, 'tail', 'right');
+subtitle(['p = ',num2str(p)])
+subplot(1,3,3); 
+polarhistogram(errResultsAll{3,1}); hold on; polarhistogram(errResultsAll{3,2});
+title('Stim error (causal - offline)'); 
+legend( ...
+    ['Constant - RMSE ',num2str(rms(errResultsAll{3,1}))], ... 
+    ['Dynamic - RMSE ',num2str(rms(errResultsAll{3,2}))], ...
+    'Location','northoutside');
+[~,p] = ttest2(errResultsAll{3,1}.^2, errResultsAll{3,2}.^2, 'tail', 'right');
+subtitle(['p = ',num2str(p)])
