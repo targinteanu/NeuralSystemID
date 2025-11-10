bc335cf4f69b9aa8574d8724db9b96658cfea359
Toren Arginteanu <targinteanu@outlook.com>
2025-11-10 02:30:00 -0500

    Update plotLTISS.m
    improved legibility

--- plotLTISS.m
+++ plotLTISS.m
@@ -1,13 +1,27 @@
 function [fig, tl] = plotLTISS(sys)
 
 A = sys.A; B = sys.B; C = sys.C; D = sys.D;
+W1 = width(A); W2 = width(B); H1 = height(A); H2 = height(C);
+
+% force min ratios to 1:5
+N = 5; 
+if W2/(W1+W2) < 1/N
+    W2 = 1; W1 = N-1;
+elseif W1/(W1+W2) < 1/N
+    W1 = 1; W2 = N-1;
+end
+if H2/(H1+H2) < 1/N
+    H2 = 1; H1 = N-1;
+elseif H1/(H1+H2) < 1/N
+    H1 = 1; H2 = N-1;
+end
 
 fig = figure; 
-tl = tiledlayout(height(A)+height(C), width(A)+width(B));
+tl = tiledlayout(H1+H2, W1+W2);
 
-nexttile([height(A), width(A)]); imagesc(A); colorbar;
-nexttile([height(B), width(B)]); imagesc(B); colorbar;
-nexttile([height(C), width(C)]); imagesc(C); colorbar;
-nexttile([height(D), width(D)]); imagesc(D); colorbar;
+nexttile([H1, W1]); imagesc(A); title('A'); colorbar;
+nexttile([H1, W2]); imagesc(B); title('B'); colorbar;
+nexttile([H2, W1]); imagesc(C); title('C'); colorbar;
+nexttile([H2, W2]); imagesc(D); title('D'); colorbar;
 
 end
