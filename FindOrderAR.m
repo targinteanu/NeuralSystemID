4bd9cd201c954a7b699207342efed01385d8998b
Toren Arginteanu <targinteanu@outlook.com>
2025-12-04 12:59:44 -0500

    Update FindOrderAR.m
    added comments and changed variable names

--- FindOrderAR.m
+++ FindOrderAR.m
@@ -1,5 +1,39 @@
-function [bestOrd, bestAIC, mdl] = ...
-    FindOrderAR(dtaBL1ch, predHzn, fbnd, doResample, maxOrd, numTests)
+function [bestOrd, bestAIC, bestMdl, rho, p] = ...
+    FindOrderAR(dta, predHzn, fbnd, doResample, maxOrd, numTests)
+
+% 
+% Experiment with different AR model orders. 
+% First, AR models of maximum order will be fit to different training sets
+% selected from the input data, and the consistency between AR coefficients
+% will be examined, with the expectation coefficients consistently close to
+% zero are beyond the useful order of the model. 
+% Next, AR models of increasing order will be fit to the same training set,
+% and the AIC of each will be computed, with the expectation that the best
+% order model will have lowest AIC. 
+% Finally, signal forecast by the selected model will be compared with
+% actual data as a measure of model accuracy. 
+% 
+% Inputs: 
+%   dta: data to model from one channel without artifact as a timetable
+%   predHzn: how much time (sec) ahead to evaluate model prediction
+%            accuracy; default 0.03
+%   fbnd: bandpass filter [low, high] frequency bounds; if empty or omitted, 
+%         no filtering will be performed 
+%   doResample: if true, data will be downsampled by 10 before fitting model
+%   maxOrd: maximum AR order to test (default 30)
+%   numTests: will attempt fitting on this many different training sets
+%             selected from the data 
+%   
+% Outputs: 
+%   bestOrd: best order defined by lowest AIC 
+%   bestAIC: BIC value of order bestOrd model
+%   bestMdl: an AR model of order bestOrd trained on all the data
+%   rho: Pearson's correlation coefficient between actual data and data
+%        predicted by the model predHzn seconds into the future. 
+%   p: p value of the alternate hypothesis that rho is nonzero. 
+% 
+
+%% manage inputs 
 
 if nargin < 6
     numTests = [];
@@ -28,34 +62,34 @@ if isempty(predHzn)
 end
 
 try 
-    Fs = dtaBL1ch.Properties.SampleRate;
+    Fs = dta.Properties.SampleRate;
 catch ME
     warning(ME.message);
     Fs = nan;
 end
 if isnan(Fs) || isempty(Fs) 
-    Fs = 1/mean(seconds(diff(dtaBL1ch.Time)));
+    Fs = 1/mean(seconds(diff(dta.Time)));
 end
 
 %% filter, downsample 
 
 FsNew = Fs;
 if ~isempty(fbnd)
-    tOrig = dtaBL1ch.Time;
+    tOrig = dta.Time;
     samplerat = 10; % replace with something based on nyquist rate
     bpf = buildFIRBPF(Fs, fbnd(1), fbnd(2));
-    dtaBL1ch = FilterTimetable(@(f,x) filtfilt(f,1,x), bpf, dtaBL1ch);
-    dtaOrig = dtaBL1ch;
+    dta = FilterTimetable(@(f,x) filtfilt(f,1,x), bpf, dta);
+    dtaOrig = dta;
     if doResample
-        dtaBL1ch = downsampleTimetable(dtaBL1ch, samplerat);
+        dta = downsampleTimetable(dta, samplerat);
         try 
-            FsNew = dtaBL1ch.Properties.SampleRate;
+            FsNew = dta.Properties.SampleRate;
         catch ME
             warning(ME.message);
             FsNew = nan;
         end
         if isnan(FsNew) || isempty(FsNew)
-            FsNew = 1/mean(seconds(diff(dtaBL1ch.Time)));
+            FsNew = 1/mean(seconds(diff(dta.Time)));
         end
     end
 end
@@ -65,19 +99,19 @@ end
 disp('Testing models on different training sets.')
 progtick = .05; prog = 0;
 
-L = floor(height(dtaBL1ch)/numTests);
+L = floor(height(dta)/numTests);
 datarat = L/maxOrd;
 disp(['Data size is ',num2str(datarat),' times variable size.'])
 
 l = ceil(L/2);
-ts = linspace(l, height(dtaBL1ch)-l, numTests);
+ts = linspace(l, height(dta)-l, numTests);
 ts = round(ts);
 
 W = nan(numTests, maxOrd+1);
 r = 1;
 for t = ts
     trng = t + [-1,1]*(l-1);
-    y = dtaBL1ch(trng(1):trng(2),:);
+    y = dta(trng(1):trng(2),:);
     mdl = ar(y, maxOrd, 'ls');
     W(r,:) = mdl.A;
     r = r+1;
@@ -130,38 +164,39 @@ xlabel('Model Order'); ylabel(AICtype);
 title('Model Information Criterion');
 
 [bestAIC,bestOrd] = min(AIC);
-mdl = ar(dtaBL1ch, bestOrd, 'yw');
+mdl = ar(dta, bestOrd, 'yw');
+bestMdl = mdl;
 
 %% forecast/predict
 
 k = ceil(predHzn * FsNew);
-LL = floor(height(dtaBL1ch)/2);
-dtaFore = myFastForecastAR(mdl, dtaBL1ch{1:LL,1}, LL);
-dtaFore = timetable(dtaBL1ch.Time(LL+(1:LL)), dtaFore);
-dtaPred = myPredict(mdl, dtaBL1ch, k, true);
+LL = floor(height(dta)/2);
+dtaFore = myFastForecastAR(mdl, dta{1:LL,1}, LL);
+dtaFore = timetable(dta.Time(LL+(1:LL)), dtaFore);
+dtaPred = myPredict(mdl, dta, k, true);
 
 figure; 
 plot(dtaOrig.Time, dtaOrig{:,1}, ':k', 'LineWidth', 1);
 hold on; grid on;
-plot(dtaBL1ch.Time, dtaBL1ch{:,1}, 'k'); 
+plot(dta.Time, dta{:,1}, 'k'); 
 plot(dtaPred.Time, dtaPred{:,1}, 'b');
 plot(dtaFore.Time, dtaFore{:,1}, 'r');
-[rho, p] = corr(dtaBL1ch{:,1}, dtaPred{:,1})
+[rho, p] = corr(dta{:,1}, dtaPred{:,1});
 
 % upsample if applicable 
 if (~isempty(fbnd)) && doResample
-    dtaBL1ch = retime(dtaBL1ch,'regular','nearest','SampleRate',Fs);
+    dta = retime(dta,'regular','nearest','SampleRate',Fs);
     dtaPred  = retime(dtaPred, 'regular','nearest','SampleRate',Fs);
     dtaFore  = retime(dtaFore, 'regular','nearest','SampleRate',Fs);
-    dtaBL1ch = FilterTimetable(@(f,x) filtfilt(f,1,x), bpf, dtaBL1ch);
+    dta = FilterTimetable(@(f,x) filtfilt(f,1,x), bpf, dta);
     dtaPred  = FilterTimetable(@(f,x) filtfilt(f,1,x), bpf, dtaPred);
     dtaFore = FilterTimetable(@(f,x) filtfilt(f,1,x), bpf, dtaFore);
 
-    plot(dtaBL1ch.Time, dtaBL1ch{:,1}, '--k'); hold on; grid on;
+    plot(dta.Time, dta{:,1}, '--k'); hold on; grid on;
     plot(dtaPred.Time, dtaPred{:,1}, '--b');
     plot(dtaFore.Time, dtaFore{:,1}, '--r');
-    [rho, p] = corr(dtaBL1ch{:,1}, dtaPred{:,1})
+    [rho, p] = corr(dta{:,1}, dtaPred{:,1});
 end
-[rho, p] = corr(dtaOrig{:,1}, dtaPred{2:end,1})
+[rho, p] = corr(dtaOrig{:,1}, dtaPred{2:end,1});
 
 end
