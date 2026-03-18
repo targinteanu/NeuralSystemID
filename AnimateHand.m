a9f3c685f822f1b56df0a88999ea94ae2dd36552
Toren Arginteanu <targinteanu@outlook.com>
2026-03-18 00:21:57 -0400

    Create AnimateHand.m
    new script to animate hand motor task from leap data

---
+++ AnimateHand.m
@@ -0,0 +1,56 @@
+function [FF, t] = AnimateHand(deviceData)
+
+t = deviceData.time; dt = [0,diff(t)];
+
+W = [deviceData.wristPosition.x; 
+     deviceData.wristPosition.y;
+     deviceData.wristPosition.z];
+P = [deviceData.palmPosition.x; 
+     deviceData.palmPosition.y;
+     deviceData.palmPosition.z];
+
+F = deviceData.f;
+F = struct2cell(F);
+F = squeeze(F);
+F = F([6,2,3,4,5],:);
+
+FF = nan(5,5,3,length(t));
+for f = 1:5
+    for j = 1:5
+        FF(j,f,1,:) = F{j,f}.x;
+        FF(j,f,2,:) = F{j,f}.y;
+        FF(j,f,3,:) = F{j,f}.z;
+    end
+end
+
+WW = nan([1,1,size(W)]); WW(:) = W(:);
+WW = repmat(WW, [1,5,1,1]);
+PP = nan([1,1,size(P)]); PP(:) = P(:);
+PP = repmat(PP, [1,5,1,1]);
+FF = [WW; PP; FF];
+
+figure; 
+
+for f = 1:5
+    Fp(f) = plot3(FF(:,f,1,1), FF(:,f,2,1), FF(:,f,3,1), '-o');
+    hold on; grid on;
+end
+x = FF(:,:,1,:); x = x(:);
+y = FF(:,:,2,:); y = y(:);
+z = FF(:,:,3,:); z = z(:);
+xlim([min(x), max(x)]);
+ylim([min(y), max(y)]);
+zlim([min(z), max(z)]);
+xlabel('x'); ylabel('y'); zlabel('z');
+
+for ti = 2:size(FF,4)
+    pause(dt(ti));
+    for f = 1:5
+        Fp(f).XData = FF(:,f,1,ti);
+        Fp(f).YData = FF(:,f,2,ti);
+        Fp(f).ZData = FF(:,f,3,ti);
+    end
+    drawnow; 
+end
+
+end
