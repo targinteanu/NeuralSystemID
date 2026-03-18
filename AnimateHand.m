55871de13d6a27404d74b7c4baef63e3810173c9
Toren Arginteanu <targinteanu@outlook.com>
2026-03-18 02:15:36 -0400

    Update AnimateHand.m
    place palm at origin, and add processing of coordinates to get angles and plot

--- AnimateHand.m
+++ AnimateHand.m
@@ -1,4 +1,5 @@
 function [FF, t] = AnimateHand(deviceData)
+%% translate struct to matrix of raw finger pos
 
 t = deviceData.time; dt = [0,diff(t)];
 
@@ -14,7 +15,7 @@ F = struct2cell(F);
 F = squeeze(F);
 F = F([6,2,3,4,5],:);
 
-FF = nan(5,5,3,length(t));
+FF = nan(5,5,3,length(t)); % joint, finger, XYZ, t
 for f = 1:5
     for j = 1:5
         FF(j,f,1,:) = F{j,f}.x;
@@ -27,7 +28,10 @@ WW = nan([1,1,size(W)]); WW(:) = W(:);
 WW = repmat(WW, [1,5,1,1]);
 PP = nan([1,1,size(P)]); PP(:) = P(:);
 PP = repmat(PP, [1,5,1,1]);
-FF = [WW; PP; FF];
+%FF = [WW; PP; FF];
+FF = [WW; FF] - PP; % place palm at origin
+
+%% animate 
 
 myfig = figure; 
 
@@ -64,5 +68,54 @@ for ti = 2:size(FF,4)
     end
     dtskip = toc(startTic);
 end
+
+%% translate raw finger pos to useful measurement 
+
+Fd = diff(FF, 1); % joint displacement 
+Fl = squeeze(sqrt(sum(Fd.^2,3))); % joint length
+Fl = median(Fl,3);
+
+% digital flexion/extension; thenar abduction/adduction
+Ftheta = nan(size(Fd,1)-1, size(Fd,2), size(Fd,3));
+for j = 2:size(Fd,1)
+    for f = 1:size(Fd,2)
+        for ti = 1:size(Fd,4)
+            dp = squeeze(Fd(j,f,:,ti))' * squeeze(Fd(j-1,f,:,ti));
+            costheta = dp / (Fl(j,f) * Fl(j-1,f));
+            costheta = max(-1, costheta); costheta = min(1, costheta);
+            Ftheta(j-1,f,ti) = acos(costheta); 
+        end
+    end
+end
+Ftheta(2,1,:) = nan; % thumb lacks final joint 
+
+FD = FF(end,:,:,:) - FF(1,:,:,:); % finger displacement 
+FD = squeeze(FD);
+FL = squeeze(sqrt(sum(FD.^2,3))); % finger length
+FL = median(FL,2);
+FD3 = squeeze(FD(3,:,:)); FD = FD([1,2,4,5],:,:); 
+FL3 = FL(3); FL = FL([1,2,4,5]);
+
+% digital absuction/adduction; thenar flexion/extension
+Fphi = nan(size(FD,1),size(FD,3));
+for f = 1:size(FD,1)
+    for ti = 1:size(FD,3)
+        dp = squeeze(FD(f,:,ti)) * FD3(:,ti);
+        costheta = dp / (FL(f) * FL3); % calculate cosine of angle
+        costheta = max(-1, min(1, costheta)); % clamp value for acos
+        Fphi(f, ti) = acos(costheta); 
+    end
+end
+
+%% plot 
+Fthetaphi = [reshape(Ftheta, [size(Ftheta,1)*size(Ftheta,2),size(Ftheta,3)]); Fphi];
+Fthetaphi = Fthetaphi';
+Fthetaphi = Fthetaphi(:, any(~isnan(Fthetaphi))); 
+Fthetaphi = Fthetaphi(dt>0,:); tthetaphi = t(dt>0);
+Ts = min(dt(dt>0)); % resample time 
+treg = tthetaphi(1):Ts:tthetaphi(end);
+Freg = interp1(tthetaphi,Fthetaphi,treg);
+figure; plot(treg, Freg); grid on; xlabel('time (s)'); ylabel('angle (rad)');
+figure; pwelch(Freg,[],[],[],1/Ts,'power'); 
 
 end
