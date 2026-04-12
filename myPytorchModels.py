f16bfb04426b14cce13d4440a9194752467ed2cb
Toren Arginteanu <targinteanu@outlook.com>
2026-04-12 15:59:34 -0400

    Update myPytorchModels.py
    fixed issues

--- myPytorchModels.py
+++ myPytorchModels.py
@@ -618,6 +618,7 @@ class TimeSeriesConv(nn.Module):
         # preprocessing features -------------------------------------------
         
         C1 = 32
+        self.C1 = C1
 
         self.num_groups = num_groups
         self.group_size = group_size
@@ -714,15 +715,16 @@ class TimeSeriesConv(nn.Module):
 
         # Stage 1
         #x_pairs = x_used.view(x_used.shape[0], x_used.shape[1], self.tuple_size, -1).permute(0,1,3,2).contiguous() # (B,T,N1,k)
-        x_pairs = x_used.view(x_used.shape[0], x_used.shape[1], self.tuple_size, -1).permute(0,3,2,1).contiguous() # (B,N1,k,T)
-        x_unpaired = x_unpaired.view(x_unpaired.shape[0], x_unpaired.shape[1], 1, x_unpaired.shape[2]).permute(0,3,2,1).contiguous() # (B,N2,1,T)
+        x_pairs = x_used.view(x_used.shape[0], x_used.shape[1], self.tuple_size, -1).permute(0,3,2,1).view(-1, self.tuple_size, x_used.shape[1]).contiguous() # (B*N1,k,T)
+        x_unpaired = x_unpaired.view(x_unpaired.shape[0], x_unpaired.shape[1], 1, x_unpaired.shape[2]).permute(0,3,2,1).view(-1, 1, x_unpaired.shape[1]).contiguous() # (B*N2,1,T)
         #p = F.gelu(self.pair_fc1(x_pairs))     # (B,T,N,C1)
-        p = F.gelu(self.pair_conv1(x_pairs))     # (B,N,C1,T')
-        x_unpaired = F.gelu(self.unpaired_conv1(x_unpaired)) # (B,N2,1,T')
+        p = F.gelu(self.pair_conv1(x_pairs))     # (B*N1,C1,T')
+        x_unpaired = F.gelu(self.unpaired_conv1(x_unpaired)) # (B*N2,1,T')
         T = T-self.K1+1 # set T<-T' after conv
-        x_unpaired = x_unpaired.permute(0,3,2,1).contiguous().view(B,T,-1) # (B,T,N2)
-        p = p.permute(0,3,1,2).contiguous() # (B,T,N1,C1)
+        x_unpaired = x_unpaired.view(B,-1,1,T).permute(0,3,2,1).view(B,T,-1).contiguous() # (B,T,N2)
+        p = p.view(B,-1,self.C1,T).permute(0,3,1,2).contiguous() # (B,T,N1,C1)
         p = F.gelu(self.pair_fc2(p))           # (B,T,N,pair_output)
+        # TO DO: need to do the same for x_left
 
         x_unpaired_groups = x_unpaired.view(x_unpaired.shape[0], x_unpaired.shape[1], self.numGrpUnpaired, -1) 
         x_unpaired_threads = x_unpaired.view(x_unpaired.shape[0], x_unpaired.shape[1], -1, self.group_size).permute(0,1,3,2).contiguous()
@@ -744,7 +746,8 @@ class TimeSeriesConv(nn.Module):
         b_list = [F.gelu(self.group_fcB[i](p_threads[:, :, i, :])) for i in range(self.group_size)]
         b = torch.stack(b_list, dim=2)
         b_flat = b.view(B, T, -1)         
-        h = torch.cat([a_flat, b_flat, c_flat, x_left], dim=-1)        # (B,T,mlp_in)
+        #h = torch.cat([a_flat, b_flat, c_flat, x_left], dim=-1)        # (B,T,mlp_in)
+        h = torch.cat([a_flat, b_flat, c_flat], dim=-1)        # (B,T,mlp_in)
 
         # Stage 3A time processing
         h = h.permute(0,2,1) # (B, dim_model, T)
