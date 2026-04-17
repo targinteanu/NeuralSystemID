aefc4e00ba27ec132f28bb6eee25fed3296c242e
Toren Arginteanu <targinteanu@outlook.com>
2026-04-16 22:49:08 -0400

    training & evaluating most recent arch

--- pytorch_ONNX_export.py
+++ pytorch_ONNX_export.py
@@ -1,14 +1,15 @@
 import torch
 import onnx
 import onnxscript
-from myPytorchModels import TimeSeriesTransformer
+from myPytorchModels import TimeSeriesConv as myNetMdl
 from csv2numpy import prepTimeSeqData
 
-netfile = "neural_network_pytorch_53b758c3e804dcbd87575a14bbf7247987a51183" # .pth -> .onnx
-numtap = 64
-groupsize=16
-numgroups=4
-dt = 0.005 # model sample time, s
+netfile = "neural_network_pytorch_f246691b075e327a9a373ae3f43229bf0b6ad320(1)" # .pth -> .onnx
+numtap = 128
+groupsize=15
+numgroups=5
+numgroupsunpaired=2
+dt = 0.01 # model sample time, s
 hzn = 1 # samples
 
 fs, feature_names, _, Xs, Ys, _, _, _, YsRaw, _, _, Us, _ = prepTimeSeqData(
@@ -18,7 +19,7 @@ Xs = torch.tensor(Xs, dtype=torch.float32)
 Ys = torch.tensor(Ys, dtype=torch.float32)
 Us = torch.tensor(Us, dtype=torch.float32)
 
-model = TimeSeriesTransformer(dim_in=Xs.shape[-1], dim_out=Ys.shape[-1], dim_u=Us.shape[-1], time_len=numtap, group_size=groupsize, num_groups=numgroups)
+model = myNetMdl(dim_in=Xs.shape[-1], dim_out=Ys.shape[-1], dim_u=Us.shape[-1], time_len=numtap, group_size=groupsize, num_groups=numgroups, tuple_size=3, numGrpUnpaired=numgroupsunpaired)
 model.load_state_dict(torch.load(netfile+".pth"))
 model.eval()
 
