579730e6b918f38876d0b731294169b450c94ff2
Toren Arginteanu <targinteanu@outlook.com>
2026-06-05 02:04:04 -0400

    Update crudeSpikeLFP.m
    make # clusters arbitrary

--- crudeSpikeLFP.m
+++ crudeSpikeLFP.m
@@ -5,11 +5,13 @@ t = seconds(Tbl.Time);
 x = Tbl.CLFP_AP_T___Central;
 Fs = 1375; % Hz
 load("/Users/torenarginteanu/Desktop/Data_PD/PD26N003/Neuro Omega/SPK_RT_SelectedTimes.mat")
-load('/Users/torenarginteanu/Desktop/Data_PD/PD26N003/Neuro Omega/spikesort_227669d9c8fe69f115266d7020ddc4244c5f3779.mat')
+load('/Users/torenarginteanu/Desktop/Data_PD/PD26N003/Neuro Omega/spikesort_227669d9c8fe69f115266d7020ddc4244c5f3779(2).mat')
 spkTbl = depth_p0496_1;
 tSpk = spkIdx/fs + seconds(spkTbl.Time(1));
-tSpk1 = spkIdx(kidx==1)/fs + seconds(spkTbl.Time(1));
-tSpk2 = spkIdx(kidx==2)/fs + seconds(spkTbl.Time(1));
+ku = unique(kidx); tSpkK = cell(size(ku));
+for ki = 1:length(ku)
+    tSpkK{ki} = spkIdx(kidx == ku(ki)) / fs + seconds(spkTbl.Time(1));
+end
 tsel = (t >= seconds(spkTbl.Time(1))) & (t <= seconds(spkTbl.Time(end)));
 x = x(tsel); t = t(tsel);
 
@@ -18,18 +20,18 @@ hpf = fir1(1024,0.25/(Fs/2),"high");
 xh = filtfilt(hpf,1,x); xl = x-xh;
 
 %% build spike train 
-z = zeros(size(t)); z1 = zeros(size(t)); z2 = zeros(size(t));
+z = zeros(size(t)); 
 for zi = tSpk'
     [~,ti] = min(abs(t-zi));
     z(ti) = 1;
 end
-for zi = tSpk1'
-    [~,ti] = min(abs(t-zi));
-    z1(ti) = 1;
-end
-for zi = tSpk2'
-    [~,ti] = min(abs(t-zi));
-    z2(ti) = 1;
+zk = cell(size(ku));
+for ki = 1:length(ku)
+    zk{ki} = zeros(size(t)); % Initialize spike train for each unique index
+    for zi = tSpkK{ki}'
+        [~, ti] = min(abs(t - zi));
+        zk{ki}(ti) = 1; % Mark spikes in the corresponding train
+    end
 end
 
 %% gaussian smooth pulse train 
@@ -46,36 +48,38 @@ w = wvals(wi);
 %}
 w = 1*Fs;
 zw = smoothdata(z,1,'gaussian',w);
-z1w = smoothdata(z1,1,'gaussian',w);
-z2w = smoothdata(z2,1,'gaussian',w);
+zkw = cellfun(@(zi) smoothdata(zi,1,'gaussian',w), zk, 'UniformOutput',false);
 
 %% compare spike and LFP signals 
 
 % normalize 
 xn = (x-mean(x))/std(x);
-z1wn = (z1w-mean(z1w))/std(z1w);
-z2wn = (z2w-mean(z2w))/std(z2w);
+zkwn = cellfun(@(zi) (zi-mean(zi))/std(zi), zkw, 'UniformOutput',false);
 zwn = (zw-mean(zw))/std(zw);
 
 % evaluate 
-r = corr(xn, zwn); r1 = corr(xn, z1wn); r2 = corr(xn, z2wn);
+r = corr(xn, zwn); 
+rr = cellfun(@(zi) corr(xn,zi), zkwn);
+lgd = string(ku)+": \rho="+string(rr);
+lgd = ["raw LFP"; "all: \rho="+string(r); lgd];
 
 % time domain 
 figure; 
-plot(t, xn); hold on; grid on; plot(t, zwn); plot(t, z1wn); plot(t, z2wn);
+plot(t, xn); hold on; grid on; plot(t, zwn); 
+for ki = 1:length(zkwn)
+    plot(t, zkwn{ki}); % Plot each spike train for comparison
+end
 title('Spike-LFP time domain comparison');
 xlabel('time (s)'); ylabel('normalized');
-legend('raw LFP', ...
-    ['all: \rho=',num2str(r)], ...
-    ['1: \rho=',num2str(r1)], ...
-    ['2: \rho=',num2str(r2)]);
+legend(lgd);
 
 % freq domain 
 figure; 
 pwelch(xn,[],[],[],Fs,'power'); hold on;
 pwelch(zwn,[],[],[],Fs,'power');
-pwelch(z1wn,[],[],[],Fs,'power'); 
-pwelch(z2wn,[],[],[],Fs,'power');
+for ki = 1:length(zkwn)
+    pwelch(zkwn{ki},[],[],[],Fs,'power');
+end
 
 %% modulated pulse train analysis 
 fc = length(spkIdx)/(t(end)-t(1)); % initial est carrier freq = avg spk rate 
