e9f4958fd63a3978efcc80c24976ed1f0795f30c
Toren Arginteanu <targinteanu@outlook.com>
2026-06-17 23:46:27 -0400

    Update pytorch_ONNX_export.py
    trying to load latest network into matlab for analysis

--- pytorch_ONNX_export.py
+++ pytorch_ONNX_export.py
@@ -4,7 +4,7 @@ import onnxscript
 from myPytorchModels import TimeSeriesConv as myNetMdl
 from csv2numpy import prepTimeSeqData
 
-netfile = "neural_network_pytorch_f246691b075e327a9a373ae3f43229bf0b6ad320(1)" # .pth -> .onnx
+netfile = "neural_network_pytorch_38757f70f4fc8aa0a179ee358c74212c1e790970" # .pth -> .onnx
 numtap = 128
 groupsize=15
 numgroups=5
@@ -13,14 +13,14 @@ dt = 0.01 # model sample time, s
 hzn = 1 # samples
 
 fs, feature_names, _, Xs, Ys, _, _, _, YsRaw, _, _, Us, _ = prepTimeSeqData(
-    seq_len=numtap, hzn_len=hzn, dt_target=dt, drawFromStart=False, maxNumel=1e8)
+    seq_len=numtap, hzn_len=hzn, dt_target=dt, drawFromStart=False, maxNumel=1e8, omitOutliers=False)
 
 Xs = torch.tensor(Xs, dtype=torch.float32)
 Ys = torch.tensor(Ys, dtype=torch.float32)
 Us = torch.tensor(Us, dtype=torch.float32)
 
 model = myNetMdl(dim_in=Xs.shape[-1], dim_out=Ys.shape[-1], dim_u=Us.shape[-1], time_len=numtap, group_size=groupsize, num_groups=numgroups, tuple_size=3, numGrpUnpaired=numgroupsunpaired)
-model.load_state_dict(torch.load(netfile+".pth"))
+model.load_state_dict(torch.load(netfile+".pth", map_location=torch.device('cpu')))
 model.eval()
 
 torch.onnx.export(
