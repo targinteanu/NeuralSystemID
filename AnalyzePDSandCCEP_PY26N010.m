b9e581403352214263204345e485721091db68a9
Toren Arginteanu <targinteanu@outlook.com>
2026-08-12 00:52:30 -0400

    Update AnalyzePDSandCCEP_PY26N010.m
    results are too influenced by artifact removal params

--- AnalyzePDSandCCEP_PY26N010.m
+++ AnalyzePDSandCCEP_PY26N010.m
@@ -17,19 +17,10 @@ T = [minutes(8),    minutes(9);    ... pk 66-65-67 (RAH2-1-3) 3mA 0.5hz
      minutes(16.5), minutes(19)];    % tr 66-65-67 4mA 0.5hz 40+trl thresh100
 t1 = datetime(2026,8,5,19,0,0); t1.TimeZone = t0.TimeZone; T = T+t1;
 tRel = seconds(t-t0);
-
-% stim timing 
 g = nstbl.AINP1; g = [false; diff(g) > 1000];
-gidx = find(g); 
 
 % art remove 
-artdur = 400; % samples 
-G = movmean(g,artdur)>eps;
-Gshift = floor(artdur/2) - 1;
-G = [false(Gshift,1); G(1:(end-Gshift))];
-tRelNoArt = tRel(~G);
-XNoArt = X(~G,:);
-XArtRem = interp1(tRelNoArt, XNoArt, tRel, 'linear', 'extrap');
+XArtRem = artremove(tRel, X, g, 400);
 
 % filter, phase 
 BPF = buildFIRBPF(fs,4,9);
@@ -45,19 +36,25 @@ end
 linkaxes(ax, 'x');
 
 % analysis 
-figure; getPhaseEP(X(:,2),Xph(:,2),g,t,[T(1,1),T(2,2)],[-250 749],fs);
+figure; getplotPhaseEP(X(:,2),Xph(:,2),g,t,[T(1,1),T(2,2)],[-250 749],fs);
 sgtitle('RAH2 peak 3mA stim RAH1-RAH3'); 
-figure; getPhaseEP(X(:,2),Xph(:,2),g,t,[T(4,1),T(4,2)],[-250 749],fs);
+figure; getplotPhaseEP(X(:,2),Xph(:,2),g,t,[T(4,1),T(4,2)],[-250 749],fs);
 sgtitle('RAH2 trough 4mA stim RAH1-RAH3'); 
-figure; getPhaseEP(X(:,1),Xph(:,1),g,t,[T(3,1),T(3,2)],[-250 749],fs);
+figure; getplotPhaseEP(X(:,1),Xph(:,1),g,t,[T(3,1),T(3,2)],[-250 749],fs);
 sgtitle('RA1 peak 4mA stim RAH1-RAH2'); 
 
 %% phase-target stim 
 % rec channel = RAH2 
-% encode: peak target; decode: trough target 
+% encode: peak target; decode (recall): trough target 
 
 nstbl = ns2timetable('/Users/torenarginteanu/Desktop/Data_EMU/PY26N010/Memory/memory001.ns2');
 evtbl = parseNEVSerialCodes('/Users/torenarginteanu/Desktop/Data_EMU/PY26N010/Memory/memory001.nev');
+%%
+fs = nstbl.Properties.SampleRate;
+t = nstbl.Time; X = nstbl.RAH2;
+t0 = t(1); 
+tRel = seconds(t-t0);
+g = nstbl.AINP1; g = [false; diff(g) > 1000];
 
 tstart = evtbl(strcmp(evtbl.EventName, 'decoding_start'),:);
 tend = evtbl(strcmp(evtbl.EventName, 'decoding_end'),:);
@@ -68,21 +65,69 @@ tstart = evtbl(strcmp(evtbl.EventName, 'image_1_adaptive'),:);
 tend = evtbl(strcmp(evtbl.EventName, 'end_encoding'),:);
 Tencode = [tstart.Time, tend.Time]; 
 
-%% helper(s)
-function getPhaseEP(x,xph,trig,t,trng,EPidx,fs)
-tEP = EPidx(1):EPidx(2); tEP = tEP/fs;
-xidx = (t > trng(1)) & (t < trng(2));
-x = x(xidx); xph = xph(xidx); trig = find(trig(xidx));
-EPl = diff(EPidx)+1;
-EP = nan(EPl,length(trig));
-for ti = 1:length(trig)
-    EP(:,ti) = x(trig(ti)+(EPidx(1):EPidx(2)));
+% art remove 
+XArtRem = artremove(tRel, X, g, 15);
+
+% filter, phase 
+BPF = buildFIRBPF(fs,4,9);
+Xf = filtfilt(BPF,1,XArtRem);
+Xph = angle(hilbert(Xf));
+figure('Theme','light'); 
+plot(t, X-mean(XArtRem), 'k','LineWidth',2); hold on; grid on; 
+plot(t, XArtRem-mean(XArtRem), 'b','LineWidth',1.75);
+plot(t, Xf, 'm','LineWidth',1.5)
+yrng = ylim();
+yrng = 0.75*yrng;
+yrng = [yrng; yrng]; yrng = yrng(:);
+for ti = 1:height(Tencode)
+    patch([Tencode(ti,:), fliplr(Tencode(ti,:))], yrng, ...
+        'r', 'FaceAlpha',0.3, 'EdgeColor','none');
 end
-bldur = 5-EPidx(1);
-EPos = EP(1:bldur,:); EPos = mean(EPos);
-EP_ = EP - EPos; 
-EPm = mean(EP_,2); EPv = std(EP_,[],2);
-ph = xph(trig); phmean = circ_mean(ph); phconf = circ_std(ph);
+for ti = 1:height(Tdecode)
+    patch([Tdecode(ti,:), fliplr(Tdecode(ti,:))], yrng, ...
+        'g', 'FaceAlpha',0.3, 'EdgeColor','none');
+end
+
+% analysis 
+phencode = []; phdecode = [];
+for ti = 1:height(Tencode)
+    [~,~,~,ph] = getPhaseEP(X,Xph,g,t, Tencode(ti,:),[],fs);
+    phencode = [phencode; ph];
+end
+for ti = 1:height(Tdecode)
+    [~,~,~,ph] = getPhaseEP(X,Xph,g,t, Tdecode(ti,:),[],fs);
+    phdecode = [phdecode; ph];
+end
+figure; sgtitle('Stim Phase');
+subplot(1,2,1); 
+polarhistogram(phencode, 12);
+phmean = circ_mean(phencode); phconf = circ_std(phencode);
+phmean = phmean*180/pi; phconf = phconf*180/pi;
+title('Encode'); subtitle([num2str(phmean),'±',num2str(phconf),'°']);
+subplot(1,2,2); 
+polarhistogram(phdecode, 12);
+phmean = circ_mean(phdecode); phconf = circ_std(phdecode);
+phmean = phmean*180/pi; phconf = phconf*180/pi;
+title('Decode'); subtitle([num2str(phmean),'±',num2str(phconf),'°']);
+
+%% helper(s)
+
+function XArtRem = artremove(tRel, X, g, artdur)
+G = movmean(g,artdur)>eps;
+Gshift = floor(artdur/2) - 1;
+G = [false(Gshift,1); G(1:(end-Gshift))];
+tRelNoArt = tRel(~G);
+XNoArt = X(~G,:);
+XArtRem = interp1(tRelNoArt, XNoArt, tRel, 'linear', 'extrap');
+end
+
+function getplotPhaseEP(x,xph,trig,t,trng,EPidx,fs)
+[tEP,EPm,EPv, ph,phmean,phconf] = ...
+    getPhaseEP(x,xph,trig,t,trng,EPidx,fs);
+plotPhaseEP(tEP,EPm,EPv, ph,phmean,phconf);
+end
+
+function plotPhaseEP(tEP,EPm,EPv, ph,phmean,phconf)
 phmean = phmean*180/pi; phconf = phconf*180/pi;
 subplot(2,1,1); polarhistogram(ph, 12);
 title('Stim Phase');
@@ -90,4 +135,25 @@ subtitle([num2str(phmean),'±',num2str(phconf),'°']);
 subplot(2,1,2); 
 errorbar(tEP,EPm,EPv); grid on;
 xlabel('sec'); ylabel('uV'); title('Evoked Potential');
+end
+
+function [tEP,EPm,EPv, ph,phmean,phconf] = ...
+    getPhaseEP(x,xph,trig,t,trng,EPidx,fs)
+xidx = (t > trng(1)) & (t < trng(2));
+x = x(xidx); xph = xph(xidx); trig = find(trig(xidx));
+if ~isempty(EPidx)
+    tEP = EPidx(1):EPidx(2); tEP = tEP/fs;
+    EPl = diff(EPidx)+1;
+    EP = nan(EPl,length(trig));
+    for ti = 1:length(trig)
+        EP(:,ti) = x(trig(ti)+(EPidx(1):EPidx(2)));
+    end
+    bldur = 5-EPidx(1);
+    EPos = EP(1:bldur,:); EPos = mean(EPos);
+    EP_ = EP - EPos; 
+    EPm = mean(EP_,2); EPv = std(EP_,[],2);
+else
+    tEP = []; EPm = []; EPv = [];
+end
+ph = xph(trig); phmean = circ_mean(ph); phconf = circ_std(ph);
 end
