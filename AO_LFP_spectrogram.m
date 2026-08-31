8ea86047774b1d8a53674bb349138187fa65e611
Toren Arginteanu <targinteanu@outlook.com>
2026-08-31 04:08:50 -0400

    Update AO_LFP_spectrogram.m
    some fixes and improvements

--- AO_LFP_spectrogram.m
+++ AO_LFP_spectrogram.m
@@ -1,25 +1,64 @@
 %% load raw data 
-load('/Users/torenarginteanu/Desktop/Data_PD/PD26N002/Neuro Omega/SavedTable1375HzRT.mat')
+
+load('/Users/torenarginteanu/Desktop/Data_PD/PD26N003/Neuro Omega/SavedTable1375HzLT.mat')
 Tbl = sortrows(Tbl, 'Time');
 %x = Tbl.CLFP_AP_T___Central;
 Fs = 1375; % Hz
-load("/Users/torenarginteanu/Desktop/Data_PD/PD26N002/Neuro Omega/SPK_RT_SelectedTimes.mat")
-spkTbl = TblD;
+load("/Users/torenarginteanu/Desktop/Data_PD/PD26N003/Neuro Omega/SPK_LT_SelectedTimes(2).mat")
+spkTbl = depth_p1886;
+
+E = Tbl.Properties.Events;
+E = E(contains(E.EventLabels, 'Stim'), :);
+D = AlphaOmegaTable2Depth(Tbl);
+
+% truncate time to match spk
+t = (Tbl.Time);
+tsel = (t >= (spkTbl.Time(1))) & (t <= (spkTbl.Time(end)));
+t = t(tsel);
+Tbl = Tbl(tsel,:); 
+Esel = (E.Time >= (spkTbl.Time(1))) & (E.Time <= (spkTbl.Time(end)));
+E = E(Esel,:);
+Dsel = (D.Time >= (spkTbl.Time(1))) & (D.Time <= (spkTbl.Time(end)));
+D = D(Dsel,:);
+
+%%
 for xch = 1:width(Tbl)
     try
-t = seconds(Tbl.Time);
-tsel = (t >= seconds(spkTbl.Time(1))) & (t <= seconds(spkTbl.Time(end)));
 x = Tbl{:,xch}; xname = Tbl.Properties.VariableNames{xch};
-x = x(tsel); t = t(tsel);
 
-% spectrogram; look for beta bursts 
+%% spectrogram; look for beta bursts 
 figure; 
-ax(1) = subplot(211);
-spectrogram(x,5*Fs,[],[],Fs,"yaxis","power"); ylim([0 200]);
+ax(1) = subplot(311);
+spectrogram(x,1*Fs,[],[],Fs,"yaxis","power"); ylim([0 200]);
 title(xname);
-ax(2) = subplot(212);
-plot(minutes(t), x); grid on; 
-linkaxes(ax, 'x');
+linkfirstax = true;
+if contains(ax(1).XLabel.String, '(s)')
+    timefun = @seconds;
+elseif contains(ax(1).XLabel.String, '(minutes)')
+    timefun = @minutes;
+elseif contains(ax(1).XLabel.String, '(hours)')
+    timefun = @hours;
+else
+    timefun = @(a) a; linkfirstax = false;
+end
+ax(2) = subplot(312);
+plot(timefun(t-t(1)), x); grid on; ylabel('LFP sig')
+ax(3) = subplot(313);
+if ~isempty(D)
+plot(timefun(D.Time-D.Time(1)), D.DEPTH); ylabel('Depth (mm)');
+end
+hold on; grid on; 
+if ~isempty(E)
+stem(timefun(E.Time-E.Time(1)), -5*ones(size(E)), '.'); 
+end
+if ~isempty(D) && ~isempty(E)
+    legend('Depth', 'Stim');
+end
+if linkfirstax
+    linkaxes(ax, 'x');
+else
+    linkaxes(ax(2:end), 'x');
+end
     catch ME
         warning(ME.message)
     end
